55d36311b46581c27790d0a11802022045ecf59b
amastaneh@gmail.com <amastaneh@gmail.com>
2015-05-06 18:21:02 +0430

    v1.0

---
+++ PersianDataAnnotationsValidator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace PersianDataAnnotations
+{
+	public class PersianDataAnnotationsValidator : DataAnnotationsModelValidator<ValidationAttribute>
+	{
+		private static Func<ValidationAttribute, string> _resourceNameFunc = attr => attr.GetType().Name;
+
+		/// <summary>
+		/// The type of the resource which holds the error messqages
+		/// </summary>
+		public static Type DefaultResourceType { get; set; }
+
+		/// <summary>
+		/// Function to get the ErrorMessageResourceName from the Attribute
+		/// </summary>
+		public static Func<ValidationAttribute, string> ResourceNameFunc
+		{
+			get { return _resourceNameFunc; }
+			set { _resourceNameFunc = value; }
+		}
+
+		public static void RegisterAdapters(Type resourceType = null)
+		{
+			Type adapterType = typeof(PersianDataAnnotationsValidator);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.CompareAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.CreditCardAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.CustomValidationAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.DataTypeAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.EmailAddressAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.EnumDataTypeAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.FileExtensionsAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.MaxLengthAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.MinLengthAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.PhoneAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.RangeAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.RegularExpressionAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.RequiredAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.StringLengthAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.UrlAttribute), adapterType);
+			DataAnnotationsModelValidatorProvider.RegisterAdapter(typeof(System.ComponentModel.DataAnnotations.ValidationAttribute), adapterType);
+			PersianDataAnnotationsValidator.DefaultResourceType = resourceType ?? typeof(DataAnnotationsResource);
+		}
+
+		public PersianDataAnnotationsValidator(ModelMetadata metadata, ControllerContext context, ValidationAttribute attribute) : base(metadata, context, attribute)
+		{
+			/// مقادیر در پراپرتی های زیر ذخیره می شوند
+			/// ولی چون فقط مقادیر خطاها مورد نظر است
+			/// نیاز به استفاده از رفلکشن نیست
+			/// attribute._defaultErrorMessage
+			/// attribute.ErrorMessageString
+
+			if (Attribute.ErrorMessageResourceType == null)
+			{
+				this.Attribute.ErrorMessageResourceType = DefaultResourceType;
+			}
+
+			if (Attribute.ErrorMessageResourceName == null)
+			{
+				this.Attribute.ErrorMessageResourceName = metadata.DataTypeName + "Attribute_ErrorMessage";
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PersianDataAnnotations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Web Design Iran")]
+[assembly: AssemblyProduct("PersianDataAnnotations")]
+[assembly: AssemblyCopyright("Copyright ©  2015 - Web Design Iran")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d230f4b7-c93a-4824-9296-47c2db8b8a09")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0")]
